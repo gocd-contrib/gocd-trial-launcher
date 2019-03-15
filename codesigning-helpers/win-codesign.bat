c22f946b517f02458f8a0b3f06be1d28b4490d76
Marques Lee <marques.lee@thoughtworks.com>
2019-03-14 22:37:02 -0700

    Make signed windows xecutable packaging a distinct step

--- codesigning-helpers/win-codesign.bat
+++ codesigning-helpers/win-codesign.bat
@@ -2,7 +2,3 @@ cd %~dp0\..\..\codesigning
 md src\win
 move %~dp0\..\..\dist\windows\amd64\run-gocd.exe src\win
 rake --trace win:sign
-dir out\win
-cd out\win
-jar --help
-jar -cMf %~dp0\..\..\win.zip run-gocd.exe -C out\win
