803e4cf66c05f9b1bb1c9c92e7cf69d736692369
Marques Lee <marques.lee@thoughtworks.com>
2019-03-20 13:07:54 -0700

    Add further dry-run capability to assembly/upload.rb and some cleanup

--- assembly/upload.rb
+++ assembly/upload.rb
@@ -8,32 +8,25 @@ ROOT_DIR = File.absolute_path(File.join(File.dirname(__FILE__), ".."))
 PKG_RE   = /^gocd-(\d+\.\d+\.\d+)-(\d+)-(\d+|localbuild)-(osx|linux|windows)\.zip$/
 
 def main(args=ARGV)
-  die "Requires exactly 1 argument; path to installers directory" if args.size != 1
+  die usage if args.size != 1
 
   installers_dir = args.first
 
-  die "installers path #{installers_dir.inspect} must be a directory" unless File.directory?(installers_dir)
+  die usage unless File.directory?(installers_dir)
 
-  FileUtils.rm_rf(File.join(ROOT_DIR, "meta"))
-  FileUtils.mkdir_p(File.join(ROOT_DIR, "meta"))
+  rm_rf(File.join(ROOT_DIR, "meta"))
+  mkdir_p(File.join(ROOT_DIR, "meta"))
 
   rel_info = create_release_metadata(installers_dir)
 
   die "No installers found!" unless rel_info.size > 0
 
-  File.open(File.join(ROOT_DIR, "meta", "latest.json"), "w") { |file|
-    file.puts rel_info.to_json
-  }
+  write_to_file(File.join(ROOT_DIR, "meta", "latest.json"), rel_info.to_json)
 
-  Dir.chdir(File.join(ROOT_DIR, "meta")) {
-    s3_sync "latest.json", "/"
-  }
+  s3_sync "latest.json", "/", working_dir: File.join(ROOT_DIR, "meta")
 
-  Dir.chdir(File.join(ROOT_DIR, "installers")) {
-    info = rel_info[rel_info.keys.first]
-
-    s3_sync ".", "installers/#{info[:version]}/#{info[:build]}/"
-  }
+  info = rel_info[rel_info.keys.first]
+  s3_sync ".", "installers/#{info[:version]}/#{info[:build]}/", working_dir: File.join(ROOT_DIR, "installers")
 end
 
 def create_release_metadata(src_dir)
@@ -49,16 +42,60 @@ def create_release_metadata(src_dir)
   }
 end
 
-def s3_sync(src, dest)
+def usage
+  %Q{USAGE:
+    File.basename(__FILE__) installers_dir
+
+    Takes exactly 1 argument:
+      installers_dir        # Directory containing installers
+
+    Required Environment Variables:
+      GOCD_UPLOAD_S3_BUCKET # S3 bucket to upload installers; NOT needed during dry-runs
+
+    Environment Toggles:
+      DRY_RUN=y             # Don't actually upload, but ok to touch filesystem (e.g., write metadata)
+      DRY_RUN=super         # Don't upload or touch the filesystem at all
+  }.strip
+end
+
+def write_to_file(path, content)
+  if super_dry?
+    dry_log "Would write to file #{path.inspect} content: #{content.inspect}"
+  else
+    File.open(path, "w") { |file|
+      file.puts content
+    }
+  end
+end
+
+def rm_rf(dir)
+  if super_dry?
+    dry_log "Would remove directory #{dir.inspect}"
+  else
+    FileUtils.rm_rf dir
+  end
+end
+
+def mkdir_p(dir)
+  if super_dry?
+    dry_log "Would create directory #{dir.inspect}"
+  else
+    FileUtils.mkdir_p dir
+  end
+end
+
+def s3_sync(src, dest, opts={working_dir: Dir.pwd})
   dest = URI.join("s3://#{getenv!("GOCD_UPLOAD_S3_BUCKET")}", dest)
   cmd = "aws s3 sync --no-progress --acl public-read --cache-control 'max-age=31536000' #{src} #{dest}"
 
   if dry_run?
-    puts "[DRY RUN] from dir: #{Dir.pwd}"
-    puts "[DRY RUN] exec: #{cmd.inspect}"
+    dry_log "From dir: #{opts[:working_dir]}"
+    dry_log "Would exec: #{cmd.inspect}"
     return true
   else
-    die "Failed to upload #{File.absolute_path(src)} to #{dest}" unless system(cmd)
+    Dir.chdir(opts[:working_dir]) {
+      die "Failed to upload #{File.absolute_path(src)} to #{dest}" unless system(cmd)
+    }
   end
 end
 
@@ -67,8 +104,16 @@ def die(msg)
   exit 1
 end
 
+def dry_log(msg)
+  STDOUT.puts("[DRY RUN] #{msg}")
+end
+
 def file_sz_in_mb(file)
   "%.1f MB" % (File.size(file).to_f / 2**20)
+end
+
+def super_dry?
+  dry_run? && "super" == ENV["DRY_RUN"]
 end
 
 def dry_run?
