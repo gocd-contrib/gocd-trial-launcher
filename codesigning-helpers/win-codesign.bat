732a9dac738dd54d3ca6ca7befad9102f7838716
Marques Lee <marques.lee@thoughtworks.com>
2019-03-26 11:18:08 -0700

    Clean up directory path

--- codesigning-helpers/win-codesign.bat
+++ codesigning-helpers/win-codesign.bat
@@ -1,5 +1,5 @@
-cd %~dp0\..\..\codesigning
+cd %~dp0..\..\codesigning
 md src\win
-move %~dp0\..\..\dist\windows\amd64\run-gocd.exe src\win
+move %~dp0..\..\dist\windows\amd64\run-gocd.exe src\win
 bundle install
 bundle exec rake --trace win:sign
