5893b238de4f0c6d30ca51fdfe9bba2e6c9185cf
Marques Lee <marques.lee@thoughtworks.com>
2019-04-18 16:47:50 -0700

    Fix path

--- codesigning-helpers/osx-codesign.sh
+++ codesigning-helpers/osx-codesign.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-cd "$(dirname "$0")"
+cd "$(dirname "$0")/../../codesigning"
 gem install bundler && bundle
 bundle exec rake --trace osx:sign_single_binary[dist/darwin/amd64/run-gocd,../../../osx-launcher.zip]
