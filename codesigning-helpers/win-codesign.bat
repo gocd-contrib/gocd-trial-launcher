d38323230c8f2f9b2a172d3cc8b6121cbe48a104
Marques Lee <marques.lee@thoughtworks.com>
2019-04-18 16:21:59 -0700

    Update codesigning helper scripts to call new codesigning rake tasks

--- codesigning-helpers/win-codesign.bat
+++ codesigning-helpers/win-codesign.bat
@@ -1,10 +1,5 @@
 :: setup
 cd %~dp0..\..\codesigning
-md src\win
-move %~dp0..\..\dist\windows\amd64\run-gocd.exe src\win
 call gem install bundler
 call bundle install
-call bundle exec rake --trace win:sign
-:: package
-cd out\win
-call jar -cMf ..\..\..\win-launcher.zip run-gocd.exe
+call bundle exec rake --trace win:sign_single_binary[..\..\dist\windows\amd64\run-gocd.exe,..\..\..\win-launcher.zip]
