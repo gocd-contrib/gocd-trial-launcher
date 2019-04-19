d43e42e225601e29ad4e60872c06aa00ca834743
Marques Lee <marques.lee@thoughtworks.com>
2019-04-18 17:54:17 -0700

    Fix codesigning?

--- codesigning-helpers/osx-codesign.sh
+++ codesigning-helpers/osx-codesign.sh
@@ -3,5 +3,4 @@
 set -e
 
 cd "$(dirname "$0")/../../codesigning"
-gem install bundler && bundle
-bundle exec rake --trace osx:sign_single_binary[dist/darwin/amd64/run-gocd,../../../osx-launcher.zip]
+rake --trace osx:sign_single_binary[dist/darwin/amd64/run-gocd,../../../osx-launcher.zip]
--- codesigning-helpers/win-codesign.bat
+++ codesigning-helpers/win-codesign.bat
@@ -1,5 +1,3 @@
 :: setup
 cd %~dp0..\..\codesigning
-call gem install bundler
-call bundle install
-call bundle exec rake --trace win:sign_single_binary[..\..\dist\windows\amd64\run-gocd.exe,..\..\..\win-launcher.zip]
+call rake --trace win:sign_single_binary[../../dist/windows/amd64/run-gocd.exe,../../../win-launcher.zip]
