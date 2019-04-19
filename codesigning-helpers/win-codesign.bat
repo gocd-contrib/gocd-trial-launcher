6c0bd14fe99af51de268305c91c7732b9752fa04
Marques Lee <marques.lee@thoughtworks.com>
2019-04-18 18:00:17 -0700

    I am going to poke my eyes out if this does not work

--- codesigning-helpers/win-codesign.bat
+++ codesigning-helpers/win-codesign.bat
@@ -1,3 +1,3 @@
 :: setup
 cd %~dp0..\..\codesigning
-call rake --trace win:sign_single_binary[../../dist/windows/amd64/run-gocd.exe,../../../win-launcher.zip]
+call rake --trace win:sign_single_binary[../dist/windows/amd64/run-gocd.exe,../../../win-launcher.zip]
