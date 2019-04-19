fe0f587b9f22c727d1d31804dfc57ecd76bb0d0e
Marques Lee <marques.lee@thoughtworks.com>
2019-04-18 18:04:34 -0700

    I'm in path hell.

--- codesigning-helpers/osx-codesign.sh
+++ codesigning-helpers/osx-codesign.sh
@@ -3,4 +3,4 @@
 set -e
 
 cd "$(dirname "$0")/../../codesigning"
-rake --trace osx:sign_single_binary[../dist/darwin/amd64/run-gocd,../../../osx-launcher.zip]
+rake --trace osx:sign_single_binary[../dist/darwin/amd64/run-gocd,../osx-launcher.zip]
